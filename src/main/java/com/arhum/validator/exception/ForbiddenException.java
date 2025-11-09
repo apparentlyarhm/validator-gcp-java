cb6b095e1a75238755fb8254895e4285d14f3a40
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-11-09 23:37:42 +0530

    Add new exception for FORBIDDEN

---
+++ src/main/java/com/arhum/validator/exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package com.arhum.validator.exception;
+
+public class ForbiddenException extends BaseException {
+    public ForbiddenException(String message, int code) {
+        super(message, code);
+    }
+}
